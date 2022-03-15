417947c67c29dc7e614ac77b51b6835d40d2af45
rainforest0 <rain_forest_00@qq.com>
2022-03-15 10:17:06 +0800

    ue create bank project

---
+++ Source/ShooterGameDemo/ShooterGameDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterGameDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAMEDEMO_API AShooterGameDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
